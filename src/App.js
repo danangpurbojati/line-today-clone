fe1fc7c6884cc19734bb45e68d41e1e794e164f7
danangpurbojati <danangpurbojati@gmail.com>
2021-03-09 09:36:52 +0700

    add jumbotron and news section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Container style={{backgroundColor: "#fff", paddingTop: 18}} maxWidth="md">
           <Navbar />
+          <Jumbotron />
         </Container>
       </BrowserRouter>
     </div>
